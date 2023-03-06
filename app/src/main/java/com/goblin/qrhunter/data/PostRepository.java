d9540fb1128a4b32f4ead55556f8829d7b98f4d3
GemmaryChang <gemmary@ualberta.ca>
2023-03-05 17:35:16 -0700

    PostRepository.java fixed

--- app/src/main/java/com/goblin/qrhunter/data/PostRepository.java
+++ app/src/main/java/com/goblin/qrhunter/data/PostRepository.java
@@ -1,20 +1,6 @@
 package com.goblin.qrhunter.data;
 
-import android.location.Location;
-
-import androidx.annotation.NonNull;
-import androidx.lifecycle.LiveData;
-
-import com.firebase.geofire.GeoFireUtils;
-import com.firebase.geofire.GeoLocation;
-import com.firebase.geofire.GeoQueryBounds;
 import com.goblin.qrhunter.Post;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.Query;
-import com.google.firebase.firestore.QuerySnapshot;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class PostRepository extends BaseRepository<Post> {
 
