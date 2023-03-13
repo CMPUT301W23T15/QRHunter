40ae50b5099eaf0526b87ccc4eb2421c2da5755e
Alvin Wu <alvinwu246@gmail.com>
2023-03-12 22:24:50 -0600

    PostRepoTest initial done

--- app/src/androidTest/java/com/goblin/qrhunter/firebase/PostRepoTest.java
+++ app/src/androidTest/java/com/goblin/qrhunter/firebase/PostRepoTest.java
@@ -40,58 +40,93 @@ import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 import com.goblin.qrhunter.Post;
 import com.google.firebase.firestore.Query;
+import com.goblin.qrhunter.QRCode;
 
 
-//    Note: as of rn, getTopPosts hasn't been used yet
+/**
+ * to run tests:
+ * firebase:emulators:start
+ *
+ */
 
-// Uncomment when implement !!!
-
-//@RunWith(AndroidJUnit4.class)
+@RunWith(AndroidJUnit4.class)
 public class PostRepoTest {
-//    Post post;
-//    PostRepository postDB;
-//    FirebaseFirestore fireStore;
-//    FirebaseAuth fireAuth;
-//    FirebaseUser user;
-//
-//    LiveData<List<Post>> data;
-//    Query q;
-//
-//    public void setUser(FirebaseUser user) {
-//        this.user = user;
-//    }
-//
-//    /**
-//     * Setup firebase to use the emulated enviroment;
-//     */
-//    @Before
-//    public void init() {
-//        fireStore = FirebaseFirestore.getInstance();
-//        fireAuth = FirebaseAuth.getInstance();
-//
-//        try {
-//            // 10.0.2.2 is the special IP address to connect to the 'localhost' of
-//            // the host computer from an Android emulator.
-//            fireStore.useEmulator("10.0.2.2", 8080);
-//            fireAuth.useEmulator("10.0.2.2", 9099);
-//
-//
-//        } catch (IllegalStateException e) {
-//            // pass
-//        } catch (Exception e) {
-//            fail("failed to setup firebase emulator is firebase emulator running");
-//        }
-//
-//
-//        FirebaseFirestoreSettings firestoreSettings = new FirebaseFirestoreSettings.Builder()
-//                .setPersistenceEnabled(false)
-//                .build();
-//        fireStore.setFirestoreSettings(firestoreSettings);
-//        postDB = new PostRepository();
-//    }
+    PostRepository postDB;
+    PlayerRepository playerDB;
+    FirebaseFirestore fireStore;
+    FirebaseAuth fireAuth;
 
-//    TODO: Need to implement the this test
-//    @Test
-//    public void getTopPostTest(){
-//
+    FirebaseUser user;
+
+    public void setUser(FirebaseUser user) {
+        this.user = user;
     }
+
+    /**
+     * Setup firebase to use the emulated enviroment;
+     */
+    @Before
+    public void init() {
+        fireStore = FirebaseFirestore.getInstance();
+        fireAuth = FirebaseAuth.getInstance();
+        try {
+            // 10.0.2.2 is the special IP address to connect to the 'localhost' of
+            // the host computer from an Android emulator.
+            fireStore.useEmulator("10.0.2.2", 8080);
+            fireAuth.useEmulator("10.0.2.2", 9099);
+
+        } catch (IllegalStateException e) {
+            // pass
+        } catch (Exception e) {
+            fail("failed to setup firebase emulator is firebase emulator running");
+        }
+
+        FirebaseFirestoreSettings firestoreSettings = new FirebaseFirestoreSettings.Builder()
+                .setPersistenceEnabled(false)
+                .build();
+        fireStore.setFirestoreSettings(firestoreSettings);
+        postDB = new PostRepository();
+        playerDB = new PlayerRepository();
+    }
+
+
+    public String addWithRandomHelper() throws ExecutionException, InterruptedException {
+        // generate random UUIDs simulates fireAuth.getCurrentUser()
+        String playerId = UUID.randomUUID().toString();
+
+        // a task to create a new user with random username
+        Task<Void> createRandomUserTask = playerDB.addWithRandomUsername(playerId);
+        // wait for task to finish
+        Tasks.await(createRandomUserTask);
+
+        // check if task failed
+        if (!createRandomUserTask.isSuccessful()) {
+            fail("addWithRandomHelper failed for unknown reason");
+        }
+        return playerId;
+    }
+    @Test
+    public void addWithRandomTest() throws ExecutionException, InterruptedException {
+        String playerId = addWithRandomHelper();
+        if(playerId == null || playerId.isEmpty()) {
+            fail("unknown failure");
+        }
+    }
+
+
+//    Both tests test that there are top posts and there is a player with a specific Post
+//    need to go more in depth with getPostByPlayerTest moving forward
+    @Test
+    public void getTopPostsTest() throws ExecutionException, InterruptedException {
+        LiveData<List<Post>> posts = postDB.getTopPosts(1);
+        assertNotNull(posts);
+    }
+
+    @Test
+    public void getPostByPlayerTest() throws ExecutionException, InterruptedException {
+        String playerId = addWithRandomHelper();
+        LiveData<List<Post>> posts = postDB.getPostByPlayer(playerId);
+        assertNotNull(posts);
+    }
+}
+
