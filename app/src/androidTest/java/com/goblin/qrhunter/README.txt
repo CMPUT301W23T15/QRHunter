8397efab0f7b86894163f4245733d65612054a83
AlvinSenWu <90438128+AlvinSenWu@users.noreply.github.com>
2023-03-14 18:25:00 -0600

    updated intent tests with new stories (#77)

---
+++ app/src/androidTest/java/com/goblin/qrhunter/README.txt
@@ -0,0 +1,10 @@
+IMPORTANT NOTEs IN TESTING
+
+Should test firebase folder with: firebase emulators:start -> then run the firebase folder for tests
+(assuming firebase login is already in)
+
+RUN THE UI FOLDER TESTS SEPERATELY WITHOUT FIREBASE -> as firebase might conflict with the robotium
+solo user with an actual firebase user, causing issues with the profile tests.
+
+If any issues when running, please email aswu@ualberta.ca for further explanation.
+- Alvin wu
--- app/src/androidTest/java/com/goblin/qrhunter/ui/MainActivityTest.java
+++ app/src/androidTest/java/com/goblin/qrhunter/ui/MainActivityTest.java
@@ -8,7 +8,6 @@ import static org.junit.Assert.assertTrue;
 import android.app.Activity;
 import android.view.KeyEvent;
 import android.widget.EditText;
-import android.widget.SearchView;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
@@ -37,17 +36,26 @@ public class MainActivityTest {
     }
 
     @Test
-    // checks to see when clicked on profile to sign out and sign back in
-    // issues with edit function -> Will crash app. Will not test edit functionality for now.
-    public void CheckProfileLogInLogOut(){
+//    REFER TO TOP IMPORTANT NOTE FOR
+    public void CheckProfile(){
         solo.assertCurrentActivity("Wrong activity", MainActivity.class);
         solo.clickOnView(solo.getView(R.id.profile_button));
+        solo.sleep(1000);
+        solo.clickOnButton("Edit");
+        solo.sleep(1000);
+        solo.enterText((EditText) solo.getView(R.id.edit_phone_number), "780-888-9821");
+        solo.clickOnView(solo.getView(R.id.button_save));
+        solo.sleep(1000);
+        solo.goBack();
+        solo.clickOnView(solo.getView(R.id.profile_button));
+        solo.sleep(1000);
+
         solo.clickOnButton("sign-out");
         solo.clickOnButton("get started");
-        solo.sleep(1000);
         solo.assertCurrentActivity("Wrong activity", MainActivity.class);
         assertTrue(solo.waitForText("sign-out"));
         solo.goBack();
+
     }
 
     @Test
@@ -79,7 +87,7 @@ public class MainActivityTest {
     }
 
     @Test
-    public void checkNavigationSearch(){
+    public void CheckNavigationSearch(){
         solo.assertCurrentActivity("wrong activity", MainActivity.class);
         solo.clickOnView(solo.getView(R.id.navigation_search));
         assertTrue(solo.waitForText("Search for other players"));
@@ -95,11 +103,14 @@ public class MainActivityTest {
         solo.sendKey(KeyEvent.KEYCODE_ENTER);
         assertTrue(solo.waitForText("user49394473"));
         solo.sleep(2000);
+        solo.clickOnView(solo.getView(R.id.searched_username));
+        solo.sleep(1000);
+        solo.goBack();
         solo.goBack();
     }
 
     @Test
-    public void checkNavSummary(){
+    public void CheckNavSummary(){
         solo.assertCurrentActivity("wrong activity", MainActivity.class);
         solo.clickOnView(solo.getView(R.id.navigation_summary));
         assertTrue(solo.waitForText("My QR Codes"));
@@ -107,16 +118,22 @@ public class MainActivityTest {
         solo.goBack();
     }
 
-    @Test
-    public void EndToEndFullTest(){
-        CheckProfileLogInLogOut();
-        CheckLeaderBoards();
-        CheckCamera();
-        CheckMap();
-        checkNavigationSearch();
-        checkNavSummary();
-    }
-    // Method 3) This method closes the activity after each test
+    /**
+     * END 2 END is commented for now, for new features -> might move into new file because may conflict with what was done
+     * Also not even sure if needed, but good to have for future
+     * RUN SPECIFICALLY IF WANT -> AS RUNNING ALL MAY CAUSE CONFLICTS BETWEEN THE DATABASE OF PLAYERS ADDED
+     */
+
+//    @Test
+//    public void EndToEndFullTest(){
+//        CheckProfile();
+//        CheckLeaderBoards();
+//        CheckCamera();
+//        CheckMap();
+//        CheckNavigationSearch();
+//        CheckNavSummary();
+//    }
+
     @After
     public void tearDown() throws Exception {
         solo.finishOpenedActivities();
